b333abe1a97e3b5d2ff37ec6ed184bc31906dd3b
cloviswusonos <yingjie.wu@sonos.com>
2021-06-11 12:21:13 -0700

    Add Hello lines

--- src/IncrementCount.java
+++ src/IncrementCount.java
@@ -1,9 +1,5 @@
 public class IncrementCount {
     public static void main(String[] args) {
         System.out.println("Hello Increment Count");
-        System.out.println("Hello Again");
-        System.out.println("Hello");
-        System.out.println("Hello A Third Times");
-        System.out.println("Hello");
     }
 }
